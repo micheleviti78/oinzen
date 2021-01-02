036657a60a4fb839c0e5a1a919fb3d7013988fa8
michele <micheleviti78@gmail.com>
2021-01-02 22:20:16 +0100

    added timer6 for HAL tick

--- core/src/hw/interrupts.c
+++ core/src/hw/interrupts.c
@@ -64,27 +64,7 @@ void UsageFault_Handler(void)
 }
 
 /**
-  * @brief This function handles System service call via SWI instruction.
-  */
-void SVC_Handler(void)
-{}
-
-/**
   * @brief This function handles Debug monitor.
   */
 void DebugMon_Handler(void)
 {}
-
-/**
-  * @brief This function handles Pendable request for system service.
-  */
-void PendSV_Handler(void)
-{}
-
-/**
-  * @brief This function handles System tick timer.
-  */
-void SysTick_Handler(void)
-{
-  HAL_IncTick();
-}
